420d35495b5cbbaec93717e317f9f9939923cc1e
venumadhavi123 <151652718+venumadhavi123@users.noreply.github.com>
2024-08-23 10:19:33 +0530

    Create Dowhiledemo.java

---
+++ Dowhiledemo.java
@@ -0,0 +1,11 @@
+import java.io.*;
+public class Dowhiledemo{
+    public static void main(String args[]){
+        int i=0;
+        do{
+            System.out.println("Hello world");
+            i++;
+        }
+        while(i<3);
+    }
+}
